12a16069b99c10da3094770988ac35e391408b7b
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-17 11:12:15 -0300

    first commit

---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,12 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Icon name="star-outline" size={50} color={'black'} />
+    </View>
+  );
+};
