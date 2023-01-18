0e9c5bdfcba450240e11e27e3bfbd1c8b4912cd3
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-18 12:25:22 -0300

    Prompt

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,7 +1,8 @@
-import {View, Text, Button, Alert} from 'react-native';
+import {View, Button, Alert} from 'react-native';
 import React from 'react';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/AppTheme';
+import prompt from 'react-native-prompt-android';
 
 export const AlertScreen = () => {
   const showAlert = () => {
@@ -25,10 +26,46 @@ export const AlertScreen = () => {
     );
   };
 
+  const showPrompt = () => {
+    // SOLO en IOS
+    // Alert.prompt(
+    //   'Prompt Title',
+    //   'Promt message',
+    //   (value: string) => console.log(`info: ${value}`),
+    //   'default',
+    //   'Hello world',
+    //   'number-pad',
+    // );
+
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
+    );
+  };
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
       <Button title="Show alert" onPress={showAlert} />
+      <View style={{height: 10}} />
+      <Button title="Show prompt" onPress={showPrompt} />
     </View>
   );
 };
